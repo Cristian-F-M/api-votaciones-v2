0cf91183b9a2d952ace515068300d27954ff8bd3
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-22 15:11:54 -0500

    refactor: Remove "Id" from the relationships

--- app/models/index.js
+++ app/models/index.js
@@ -4,16 +4,17 @@ import Role from './Role.js'
 import TypeDocument from './TypeDocument.js'
 import User from './User.js'
 import Session from './Session.js'
+import Config from './Config.js'
 
-Function.hasOne(CategoryFunctions, { foreignKey: 'functionId' })
-CategoryFunctions.belongsTo(Function, { foreignKey: 'functionId' })
-Role.hasMany(Function, { foreignKey: 'roleId' })
-Function.belongsTo(Role, { foreignKey: 'roleId' })
-Role.hasMany(User, { foreignKey: 'roleId' })
-User.belongsTo(Role, { foreignKey: 'roleId' })
-Session.hasMany(User, { foreignKey: 'sessionId' })
-User.belongsTo(Session, { foreignKey: 'sessionId' })
-TypeDocument.hasMany(User, { foreignKey: 'typeDocumentId' })
-User.belongsTo(TypeDocument, { foreignKey: 'typeDocumentId' })
+Function.hasOne(CategoryFunctions, { foreignKey: 'function' })
+CategoryFunctions.belongsTo(Function, { foreignKey: 'function' })
+Role.hasMany(Function, { foreignKey: 'role' })
+Function.belongsTo(Role, { foreignKey: 'role' })
+Role.hasMany(User, { foreignKey: 'role' })
+User.belongsTo(Role, { foreignKey: 'role' })
+Session.hasMany(User, { foreignKey: 'session' })
+User.belongsTo(Session, { foreignKey: 'session' })
+TypeDocument.hasMany(User, { foreignKey: 'typeDocument' })
+User.belongsTo(TypeDocument, { foreignKey: 'typeDocument' })
 
-export { CategoryFunctions, Role, Function, TypeDocument, User, Session }
+export { CategoryFunctions, Role, Function, TypeDocument, User, Session, Config }
