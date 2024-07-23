47aa7a5dfe0024ec0484f73ff39bfeaf33306471
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-23 16:15:33 -0500

    refactor: Remove session model and relationships from models/index.js

--- app/models/index.js
+++ app/models/index.js
@@ -3,7 +3,6 @@ import Function from './Function.js'
 import Role from './Role.js'
 import TypeDocument from './TypeDocument.js'
 import User from './User.js'
-import Session from './Session.js'
 import Config from './Config.js'
 
 Function.hasOne(CategoryFunctions, { foreignKey: 'function' })
@@ -12,9 +11,7 @@ Role.hasMany(Function, { foreignKey: 'role' })
 Function.belongsTo(Role, { foreignKey: 'role' })
 Role.hasMany(User, { foreignKey: 'role' })
 User.belongsTo(Role, { foreignKey: 'role' })
-Session.hasMany(User, { foreignKey: 'session' })
-User.belongsTo(Session, { foreignKey: 'session' })
 TypeDocument.hasMany(User, { foreignKey: 'typeDocument' })
 User.belongsTo(TypeDocument, { foreignKey: 'typeDocument' })
 
-export { CategoryFunctions, Role, Function, TypeDocument, User, Session, Config }
+export { CategoryFunctions, Role, Function, TypeDocument, User, Config }
