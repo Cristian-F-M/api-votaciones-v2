0b2926d87036cf93f473e2957e0dfd1c663669f5
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-23 16:23:40 -0500

    chore: Add cookie-parser to app express
    refactor: Uncomment import from auth route

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-// import { auth } from './routes/index.js';
+import cookieParser from 'cookie-parser'
+import { auth } from './routes/index.js'
 
 export default function createApp () {
   const app = express()
 
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(cookieParser())
 
   // ? Hacerlo para las demás rutas
-  // app.use('/', auth)
+  app.use('/', auth)
 
   return app
 }
