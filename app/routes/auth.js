a375ae0b1b3f49431534c934b74d9acabf85b858
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-23 16:11:43 -0500

    chore: Add / route

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,9 +1,25 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const auth = express.Router()
 
-auth.get('/', (req, res) => {
-  res.send('Hello auth!')
+auth.get('/', async (req, res) => {
+  const { token } = req.cookies
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  if (!token) {
+    res.status(401).json({ message: 'You need to be logged in' })
+    return
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ message: 'You need to be logged in' })
+      return
+    }
+    res.json({ message: 'You are logged in', user: decoded })
+  })
 })
 
 export default auth
