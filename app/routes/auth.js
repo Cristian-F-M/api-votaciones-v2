c24a9101af35df983a0729210faea2b3ffd8e21f
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-23 16:12:05 -0500

    chore: Add /Register route

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,5 +1,10 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import { registerValidate } from '../middlewares/validationUserMiddleware.js'
+import { registerValidation } from '../validators/userValidators.js'
+import { Role, User } from '../models/index.js'
+import bcrypt from 'bcrypt'
+import { groupBy } from '../lib/fields.js'
 
 const auth = express.Router()
 
@@ -20,6 +25,32 @@ auth.get('/', async (req, res) => {
     }
     res.json({ message: 'You are logged in', user: decoded })
   })
+})
+
+auth.post('/Register', registerValidation, registerValidate, async (req, res) => {
+  const { name, lastname, typeDocument, document, phone, email, password } = req.body
+
+  const role = await Role.findOne({ where: { code: 'User' } })
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  try {
+    await User.create({
+      name,
+      lastname,
+      typeDocument,
+      document,
+      phone,
+      email,
+      role: role.id,
+      password: bcrypt.hashSync(password, 10)
+    })
+  } catch (err) {
+    const groupedErrors = groupBy(err.errors, err => err.path)
+    res.status(400).json({ groupedErrors })
+    return
+  }
+  res.json({ message: 'Registered' })
 })
 
 export default auth
