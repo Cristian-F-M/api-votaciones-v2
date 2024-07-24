2e8d608ce4279cf49ba2fb917eba320508cb9663
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-24 12:54:49 -0500

    chore: Add Login route

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import { validateUser } from '../middlewares/UserMiddlewares.js'
-import { registerValidation } from '../validators/userValidators.js'
+import { validateUser, verifyToken } from '../middlewares/UserMiddlewares.js'
+import { loginValidation, registerValidation } from '../validators/userValidators.js'
 import { Role, User } from '../models/index.js'
 import bcrypt from 'bcrypt'
 import { groupBy } from '../lib/fields.js'
+import Session from '../models/Session.js'
 
 const auth = express.Router()
 
@@ -37,6 +38,41 @@ auth.post('/Register', registerValidation, validateUser, async (req, res) => {
     res.status(400).json({ message: 'An error has occurred, please try again' })
   }
   res.json({ message: 'Registered' })
+})
+
+auth.post('/Login', loginValidation, validateUser, verifyToken, async (req, res) => {
+  const { typeDocument, document, password } = req.body
+  const user = await User.findOne({ where: { typeDocument, document } })
+
+  if (!user) {
+    res.status(401).json({ message: 'Incorrect credentials' })
+    return
+  }
+
+  if (!bcrypt.compareSync(password, user.password)) {
+    res.status(401).json({ message: 'Incorrect credentials' })
+    return
+  }
+
+  try {
+    const token = jwt.sign({ id: user.id, name: user.name, lastname: user.lastname, email: user.email }, process.env.JWT_SECRET, { expiresIn: '4h' })
+
+    const session = await Session.create(
+      {
+        token,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 6)
+      }
+    )
+
+    user.session = session.id
+    await user.save()
+
+    res.cookie('token', token, { httpOnly: true, secure: true, maxAge: 14400000 })
+    res.json({ message: 'Now you are logged in' })
+  } catch (err) {
+    console.log(err)
+    res.status(401).json({ message: 'An error has occurred, please try again' })
+  }
 })
 
 export default auth
