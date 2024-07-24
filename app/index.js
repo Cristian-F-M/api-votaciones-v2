1738258345d493ddd175e03974119ada8277df6c
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-24 17:56:03 -0500

    chore: Add cors config

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import { auth } from './routes/index.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default function createApp () {
   const app = express()
@@ -9,6 +13,9 @@ export default function createApp () {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(cookieParser())
+  app.use(cors({
+    origin: process.env.ALLOWED_ORIGIN
+  }))
 
   // ? Hacerlo para las demás rutas
   app.use('/', auth)
