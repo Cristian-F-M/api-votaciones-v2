59915dafd20b3762bfdef5d63b83eaf298d94bc2
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-24 12:44:36 -0500

    refactor: Remove unuse code to use verifyToken middleware

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -8,23 +8,8 @@ import { groupBy } from '../lib/fields.js'
 
 const auth = express.Router()
 
-auth.get('/', async (req, res) => {
-  const { token } = req.cookies
-
-  res.setHeader('Access-Control-Allow-Origin', '*')
-
-  if (!token) {
-    res.status(401).json({ message: 'You need to be logged in' })
-    return
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      res.status(401).json({ message: 'You need to be logged in' })
-      return
-    }
-    res.json({ message: 'You are logged in', user: decoded })
-  })
+auth.get('/', verifyToken, async (req, res) => {
+  res.status(401).json({ message: 'You needd to be logged in' })
 })
 
 auth.post('/Register', registerValidation, validateUser, async (req, res) => {
