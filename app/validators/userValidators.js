6f72faa1f20859fa0c397c20e5d4c2d8015c45cc
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-07-30 19:16:36 -0500

    chore: Rename typeDocument to typeDocumentCode in user validation logic

--- app/validators/userValidators.js
+++ app/validators/userValidators.js
@@ -3,7 +3,7 @@ import { body } from 'express-validator'
 export const registerValidation = [
   body('name').notEmpty().withMessage('Name is required').isLength({ min: 3, max: 20 }).withMessage('Name must be between 3 and 20 characters long'),
   body('lastname').notEmpty().withMessage('Lastname is required').isLength({ min: 3, max: 25 }).withMessage('Lastname must be between 3 and 25 characters long'),
-  body('typeDocument').notEmpty().withMessage('Type document is required'),
+  body('typeDocumentCode').notEmpty().withMessage('Type document is required'),
   body('document').notEmpty().withMessage('Document is required').isLength({ min: 7, max: 15 }).withMessage('Document must be between 7 and 15 characters long'),
   body('phone').notEmpty().withMessage('Phone is required').isLength({ min: 10, max: 10 }).withMessage('Phone must be between 10 and 15 characters long'),
   body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Email must be a valid email'),
@@ -11,7 +11,7 @@ export const registerValidation = [
 ]
 
 export const loginValidation = [
-  body('typeDocument').notEmpty().withMessage('Type of document is required'),
+  body('typeDocumentCode').notEmpty().withMessage('Type of document is required'),
   body('document').notEmpty().withMessage('Document is required'),
   body('password').notEmpty().withMessage('Password is required')
 ]
