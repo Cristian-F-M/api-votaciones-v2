f112a612a49d37711b1b597cc172c2c3fbd29c60
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-30 18:09:53 -0500

    Add Vote model

---
+++ app/models/Vote.js
@@ -0,0 +1,41 @@
+import sequelize from '../config/database.js'
+import { DataTypes } from 'sequelize'
+
+const Vote = sequelize.define('Vote', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'User',
+      key: 'id'
+    }
+  },
+  cantVotes: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  totalVotes: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+},
+{
+  timestamps: false
+})
+
+export default Vote
