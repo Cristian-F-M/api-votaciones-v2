cf5941c2553d1c69d2c0f80a1b2a74f8efa4fab5
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-30 18:08:22 -0500

    chore: Update index.js to remove unused imports

--- app/models/index.js
+++ app/models/index.js
@@ -1,15 +1,9 @@
-import CategoryFunctions from './CategoryFunctions.js'
-import Function from './Function.js'
 import Role from './Role.js'
 import TypeDocument from './TypeDocument.js'
 import User from './User.js'
 import Config from './Config.js'
 import Session from './Session.js'
 
-Function.hasOne(CategoryFunctions, { foreignKey: 'function' })
-CategoryFunctions.belongsTo(Function, { foreignKey: 'function' })
-Role.hasMany(Function, { foreignKey: 'role' })
-Function.belongsTo(Role, { foreignKey: 'role' })
 Role.hasMany(User, { foreignKey: 'role' })
 User.belongsTo(Role, { as: 'roleUser', foreignKey: 'role' })
 TypeDocument.hasMany(User, { foreignKey: 'typeDocument' })
@@ -17,4 +11,4 @@ User.belongsTo(TypeDocument, { as: 'typeDocumentUser', foreignKey: 'typeDocument
 Session.hasOne(User, { foreignKey: 'session' })
 User.belongsTo(Session, { foreignKey: 'session' })
 
-export { CategoryFunctions, Role, Function, TypeDocument, User, Config, Session }
+export { Role, TypeDocument, User, Config, Session }
