e5656e82011fafba7ccd850817435ba9c6fbd5ac
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-10-30 18:07:10 -0500

    chore: Remove unused imports from syncDb.js

--- app/config/syncDb.js
+++ app/config/syncDb.js
@@ -1,12 +1,12 @@
 import sequelize from './database.js'
 // eslint-disable-next-line no-unused-vars
-import { CategoryFunctions, Function, Role, TypeDocument, User, Config } from '../models/index.js'
+import { Role, TypeDocument, User, Config } from '../models/index.js'
 
 function setupDb () {
   sequelize
     .sync({ force: true })
     .then(() => {
-      console.log('\nDatabase synced!!!!\n')
+      console.log('\nDatabase synced!!!!')
     })
     .catch((err) => {
       console.error('Error syncing database:', err)
