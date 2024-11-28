6e18e21e133ce3c3e8a9ea3730d8bdf9c5219602
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-11-28 16:12:36 -0500

    feat: Add vote and candidate routes to index.js for enhanced voting functionality and candidate management

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,5 +2,7 @@ import auth from './auth.js'
 import config from './config.js'
 import typeDocument from './typeDocument.js'
 import user from './user.js'
+import vote from './vote.js'
+import candidate from './candidate.js'
 
-export { auth, config, typeDocument, user }
+export { auth, config, typeDocument, user, vote, candidate }
