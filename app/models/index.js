b70dfa870431f99501542bc060fe665c73cb1bbc
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-11-28 16:10:31 -0500

    chore: Update associations in index.js to include Candidate model and adjust User relationships for better data integrity

--- app/models/index.js
+++ app/models/index.js
@@ -4,13 +4,16 @@ import User from './User.js'
 import Config from './Config.js'
 import Session from './Session.js'
 import Vote from './Vote.js'
+import Candidate from './Candidate.js'
 
-Role.hasMany(User, { foreignKey: 'role' })
+Role.belongsTo(User, { foreignKey: 'role' })
 User.belongsTo(Role, { as: 'roleUser', foreignKey: 'role' })
-TypeDocument.hasMany(User, { foreignKey: 'typeDocument' })
+TypeDocument.belongsTo(User, { foreignKey: 'typeDocument' })
 User.belongsTo(TypeDocument, { as: 'typeDocumentUser', foreignKey: 'typeDocument' })
 Session.hasOne(User, { foreignKey: 'session' })
 User.belongsTo(Session, { foreignKey: 'session' })
 Vote.belongsTo(User, { foreignKey: 'userId' })
+Candidate.belongsTo(User, { as: 'user', foreignKey: 'userId' })
+User.belongsTo(Candidate, { foreignKey: 'userId' })
 
-export { Role, TypeDocument, User, Config, Session }
+export { Role, TypeDocument, User, Config, Session, Candidate }
