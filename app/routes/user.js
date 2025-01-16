38516f987ce4844b323008d2764eabee5790859c
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-01-16 15:39:33 -0500

    chore: Implement /sendNotification route for authorized users to send notifications through Expo service

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,6 +4,8 @@ import { verifyToken2 } from '../middlewares/UserMiddlewares.js'
 
 const user = express.Router()
 
+const EXPO_NOTIFICATION_URL = process.env.EXPO_NOTIFICATION_URL
+
 user.get('/', verifyToken2, async (req, res) => {
   const { userId } = req.headers
   const user = await User.findByPk(userId, {
@@ -34,6 +36,42 @@ user.post('/notificationToken', verifyToken2, async (req, res) => {
   await user.save()
 
   res.json({ ok: true, message: 'Token de notificaciones actualizado' })
+})
+
+user.post('/sendNotification', verifyToken2, async (req, res) => {
+  const { userId } = req.headers
+  const user = await User.findByPk(userId, {
+    include: [
+      { model: Role, as: 'roleUser', attributes: ['id', 'name', 'code'] },
+      { model: TypeDocument, as: 'typeDocumentUser', attributes: ['id', 'name', 'code'] }
+    ]
+  })
+
+  if (!user) return res.json({ ok: false, message: 'Usuario no encontrado' })
+
+  if (user.roleUser.code !== 'Administrator') return res.json({ ok: false, message: 'No tiene permisos para enviar notificaciones' })
+
+  const { to, title, body } = req.body
+
+  const res2 = await fetch(EXPO_NOTIFICATION_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      to,
+      sound: 'default',
+      title,
+      body
+    })
+  })
+
+  const { data } = await res2.json()
+
+  if (data.status !== 'ok') return res.json({ ok: false, message: 'Error al enviar notificacion' })
+
+  res.json({ ok: true, message: 'Notificacion enviada' })
 })
 
 export default user
