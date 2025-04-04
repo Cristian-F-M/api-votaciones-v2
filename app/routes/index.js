baf30f569f42004da715935e9f493b1790218e27
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-04-04 17:04:33 -0500

    feat: Integrate assets route into index for improved asset management and server functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,6 @@ import user from './user.js'
 import vote from './vote.js'
 import candidate from './candidate.js'
 import role from './role.js'
+import assets from './assets.js'
 
-export { auth, config, typeDocument, user, vote, candidate, role }
+export { auth, config, typeDocument, user, vote, candidate, role, assets }
