fce1536ad66cf1a8fdb333a3a6cbfb5ca75f881c
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-05-03 15:03:33 -0500

    refactor: Update method and validations on the typeDocument/ route

--- app/routes/typeDocument.ts
+++ app/routes/typeDocument.ts
@@ -6,35 +6,33 @@ const typeDocument = express.Router()
 typeDocument.get('/', async (req, res) => {
 	const { code } = req.query
 
-	if (!code || typeof code !== 'string') {
+	if (!code) {
+		const typesDocuments = await TypeDocument.findAll()
+		res.json({ ok: true, typesDocuments })
+		return
+	}
+
+	if (typeof code !== 'string') {
 		res
 			.status(400)
-			.json({ ok: false, message: 'Parametro de busqueda incorrecto' })
+			.json({ ok: false, message: 'Parametro de busqueda incorrecto', code })
 		return
 	}
 
-	if (code) {
-		const typeDocument = await TypeDocument.findOne({
-			where: { code },
-		})
+	const typeDocument = await TypeDocument.findOne({
+		where: {
+			code,
+		},
+	})
 
-		if (!typeDocument) {
-			res.json({ ok: false, message: 'Type of document not found' })
-			return
-		}
-
-		res.json({ ok: true, typeDocument })
+	if (!typeDocument) {
+		res
+			.status(404)
+			.json({ ok: false, message: 'Tipo de documento no encontrado' })
 		return
 	}
 
-	const typesDocuments = await TypeDocument.findAll()
-
-	if (!typesDocuments) {
-		res.json({ ok: false, message: 'Types of documents not found' })
-		return
-	}
-
-	res.json({ ok: true, typesDocuments })
+	res.json({ ok: true, typesDocuments: [typeDocument] })
 })
 
 export default typeDocument
