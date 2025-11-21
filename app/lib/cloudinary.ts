9138f593972a355fa8c7894eb225b42a7c2df0c2
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-11-21 17:17:07 -0500

    Add function to upload imagen to cloud

---
+++ app/lib/cloudinary.ts
@@ -0,0 +1,26 @@
+import type { UploadApiResponse, UploadApiOptions } from 'cloudinary'
+import cloudinary from '@/config/cloudinary'
+
+const defaultOptions: Partial<UploadApiOptions> = {
+	folder: 'votaciones-v2/users',
+	overwrite: true,
+	use_filename: false,
+	unique_filename: false,
+	resource_type: 'auto',
+	type: 'upload',
+}
+
+export async function uploadImage(filePath: string, options?: Partial<UploadApiOptions>) {
+	const notNullOptions = Object.fromEntries(Object.entries(options ?? {}).filter(([_, v]) => v != null))
+
+	const uploadOpions = { ...defaultOptions, ...notNullOptions }
+
+	try {
+		const result = await cloudinary.uploader.upload(filePath, uploadOpions)
+
+		return { ok: true, result }
+	} catch (error) {
+		console.error('❌ Error subiendo la imagen:', error)
+		return { ok: false, message: 'Error subiendo la imagen' }
+	}
+}
