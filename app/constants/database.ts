c70949dba878faf2e29229f3cc6417a5be76d6fa
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-09 11:16:03 -0500

    Add seed constants

---
+++ app/constants/database.ts
@@ -0,0 +1,75 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const { CM_PASSWORD } = process.env
+
+export const BLANK_VOTE_USER = {
+	document: '0',
+	email: 'voto@votaciones.com',
+	phone: '0',
+	name: 'Voto en blanco',
+	lastname: '',
+	description: 'Vota en blanco si no estás de acuerdo con ninguno de los candidatos',
+	password: CM_PASSWORD,
+}
+
+export const TYPES_DOCUMENTS = {
+	CEDULA_CIUDADANIA: {
+		name: 'Cédula de ciudadanía',
+		code: 'CedulaCiudadania',
+		description:
+			'Documento de identidad emitido a los ciudadanos colombianos mayores de 18 años para acreditar su ciudadanía',
+	},
+	TARJETA_IDENTIDAD: {
+		name: 'Tarjeta de identidad',
+		code: 'TarjetaIdentidad',
+		description: 'Documento emitido a los ciudadanos colombianos mayores de 7 años para acreditar su identidad',
+	},
+	CEDULA_EXTRANJERIA: {
+		name: 'Cédula de extranjería',
+		code: 'CedulaExtranjeria',
+		description: 'Documento emitido a los ciudadanos extranjeros para acreditar su ciudadanía',
+	},
+	PASAPORTE: {
+		name: 'Pasaporte',
+		code: 'Passport',
+		description: 'Documento con validez internacional expedido por las autoridades de su respectivo país',
+	},
+}
+
+export const ROLES = {
+	USER: {
+		name: 'Usuario',
+		code: 'User',
+		description: 'Usuario de la aplicación',
+	},
+	APPRENTICE: {
+		name: 'Aprendiz',
+		code: 'Apprentice',
+		description: 'Usuario que se encuentra registrado en el sistema y tiene acceso a las funciones de aprendizaje',
+	},
+	ADMINISTRATOR: {
+		name: 'Administrador',
+		code: 'Administrator',
+		description: 'Usuario que tiene acceso a la administración de la aplicación',
+	},
+	DEVELOPER: {
+		name: 'Desarrollador',
+		code: 'Developer',
+		description: 'Desarrollador de la aplicación',
+	},
+	CANDIDATE: {
+		name: 'Candidato',
+		code: 'Candidate',
+		description: 'Usuario candidato a votar',
+	},
+}
+
+export const CONFIGS = {
+	COLOR: {
+		name: 'Color',
+		code: 'color',
+		description: 'Color of the logo and the main texts',
+		value: '#ff6719',
+	},
+}
