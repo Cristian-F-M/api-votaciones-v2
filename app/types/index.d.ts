a64f327a037f14310fe4fc7f25d9b20338382acf
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-10 17:06:57 -0500

    Add ALLOWED_SESSION_TYPE based on constant variable (ALLOWED_SESSION_TYPES)

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,4 +1,6 @@
 import type { User } from '@/types/models'
 import type { Request } from 'express'
+import type { ALLOWED_SESSION_TYPES } from '@/constants/database'
 
 export type CustomRequest<T extends Record<string, unknown>> = Request & T
+export type ALLOWED_SESSION_TYPE = keyof typeof ALLOWED_SESSION_TYPES
