0fabbde1a555e0a372ec9d45a579e379e8ebe817
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-10 19:21:16 -0500

    Add function to remove sensitive information from errors object and group them by path

--- app/lib/fields.ts
+++ app/lib/fields.ts
@@ -1,1 +1,14 @@
+import type { ValidationError } from 'sequelize'
+
 export const groupBy = Object.groupBy
+
+export function getCleanedSequelizeErrors(validationError: ValidationError) {
+	const errors = validationError.errors
+	const filteredErrors = errors.map((err) => {
+		const { message, path, type } = err
+
+		return { message, path, type }
+	})
+
+	return Object.groupBy(filteredErrors, (o) => o.path || 'unknown')
+}
