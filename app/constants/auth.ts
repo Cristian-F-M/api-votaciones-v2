d79c8d54bd709fb85824ec726f19e2483cf11233
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-11 18:32:02 -0500

    Add expiration time constant for password reset code

--- app/constants/auth.ts
+++ app/constants/auth.ts
@@ -3,3 +3,6 @@ export const SESSION_EXPIRATION_TIME = 30 * 24 * 3600
 
 /** 2 hours expressed in seconds */
 export const TOKEN_EXPIRATION_TIME = 12 * 60 * 60
+
+/** 6 hours expressed in seconds */
+export const RESET_PASSWORD_CODE_EXPIRATION_TIME = 6 * 60 * 60
