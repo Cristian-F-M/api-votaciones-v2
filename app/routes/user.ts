1efc1a27654684fa19492684a64604faf9ea11fc
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-12 08:18:09 -0500

    Update user routes

--- app/routes/user.ts
+++ app/routes/user.ts
@@ -1,473 +1,172 @@
 import express from 'express'
 import type { Request, Response } from 'express'
-import { Role, TypeDocument, User } from '@/models/index'
-import { roleRequired, validateUser, verifyToken2 } from '@/middlewares/UserMiddlewares'
+import { DeviceToken, Profile } from '@/models/index'
+import { roleRequired, sessionRequired, validateRequest } from '@/middlewares/UserMiddlewares'
 import fs from 'node:fs'
-import {
-	updateProfileValidation,
-	findUserValidation,
-	verifyPasswordResetCodeValidation,
-	sendResetCodeValidation,
-	updatePasswordValidation,
-	notifyValidation,
-} from '@/validators/userValidators'
+import { updateProfileValidation, notificationTokenValidation } from '@/validators/userValidators'
 import multer from 'multer'
-import { getPasswordResetCode, getSecretEmail } from '@/lib/user'
-import bcrypt from 'bcrypt'
 import pLimit from 'p-limit'
 import { uploadImage } from '@/lib/cloudinary'
 import path from 'node:path'
+import type { RequestWithUser } from '@/types/auth'
 
 const user = express.Router()
 const limit = pLimit(6)
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => cb(null, 'app/assets/images/profiles'),
-  filename: (req, file, cb) => cb(null, file.originalname)
+	filename: (req, file, cb) => cb(null, file.originalname)
 })
 const upload = multer({ storage })
-const imagesUrl = '.\\app\\assets\\images'
-
 const EXPO_NOTIFICATION_URL = process.env.EXPO_NOTIFICATION_URL
 
-user.get('/', verifyToken2, async (req, res) => {
-	const { userId } = req.headers
+user.get('/', sessionRequired, async (req: Request, res: Response) => {
+	// TODO -> Buscar una mejor forma de hacer 👇
+	// TODO -> Eliminar los timestamps
+	const {
+		id,
+		email,
+		document,
+		typeDocument,
+		role,
+		profile: { name, lastname, phone, imageUrl },
+		roleId,
+		typeDocumentId
+	} = (req as RequestWithUser).user
 
-	if (!userId || typeof userId !== 'string') {
-		res
-			.status(400)
-			.json({ ok: false, message: 'Parametro de busqueda incorrecto' })
-		return
+	const typeDocumentObj = {
+		id: typeDocument.id,
+		name: typeDocument.name
 	}
 
-	const user = await User.findByPk(userId, {
-		include: [
-			{ model: Role, as: 'roleUser', attributes: ['id', 'name', 'code'] },
-			{
-				model: TypeDocument,
-				as: 'typeDocumentUser',
-				attributes: ['id', 'name', 'code'],
-			},
-		],
-		attributes: [
-			'id',
-			'name',
-			'lastname',
-			'document',
-			'email',
-			'phone',
-			'voted',
-			'votedCandidateId',
-			'imageUrl',
-		],
-	})
-
-	if (!user) {
-		res.status(404).json({ ok: false, error: 'User not found' })
-		return
+	const roleObj = {
+		id: role.id,
+		name: role.name
 	}
 
-	res.json({ ok: true, user })
+	const profileObj = { name, lastname, phone, imageUrl }
+
+	const userObject = {
+		id,
+		email,
+		document,
+		typeDocumentId,
+		typeDocument: typeDocumentObj,
+		roleId,
+		role: roleObj,
+		profile: profileObj
+	}
+
+	res.json({ ok: true, user: userObject })
 })
 
-user.post('/notificationToken', verifyToken2, async (req, res) => {
-	const { userId } = req.headers
+user.patch(
+	'/notification-token',
+	sessionRequired,
+	validateRequest(notificationTokenValidation),
+	async (req: Request, res: Response) => {
+		const { notificationToken, deviceType } = req.body
+		const user = (req as RequestWithUser).user
+		const { 'session-type': sessionType } = req.headers
 
-	if (!userId || typeof userId !== 'string') {
-		res
-			.status(400)
-			.json({ ok: false, message: 'Parametro de busqueda incorrecto' })
-		return
+		if (sessionType !== 'MOBILE') {
+			res.status(400).json({
+				ok: false,
+				message: 'Las notificaciones mediante un token solo están disponibles los dispositivos móviles'
+			})
+			return
+		}
+
+		try {
+			const [deviceToken, created] = await DeviceToken.findOrCreate({
+				where: {
+					userId: user.id,
+					deviceType
+				},
+				defaults: {
+					deviceType,
+					isActive: true,
+					token: notificationToken,
+					userId: user.id
+				}
+			})
+
+			if (!created) await deviceToken.update({ isActive: true, token: notificationToken })
+		} catch (err) {
+			console.log(err)
+
+			res
+				.status(500)
+				.json({ ok: false, message: 'Ocurrio un error actualizando las notificaciones, por favor intentalo más tarde' })
+			return
+		}
+
+		res.json({ ok: true, message: 'Se han actualizado corretamente las notificaciones' })
 	}
+)
 
-	const user = await User.findByPk(userId)
+user.post('/send-notification', sessionRequired, roleRequired('ADMINISTRATOR'), (req: Request, res: Response) => {
+	const user = (req as RequestWithUser).user
 
-	if (!user) {
-		res.json({ ok: false, message: 'Usuario no encontrado' })
-		return
-	}
-
-	const { notificationToken } = req.body
-
-	user.notificationToken = notificationToken
-	await user.save()
-
-	res.json({ ok: true, message: 'Token de notificaciones actualizado' })
-})
-
-user.post('/sendNotification', verifyToken2, roleRequired('Administrator'), async (req, res) => {
-	const { userId } = req.headers
-
-	if (!userId || typeof userId !== 'string') {
-		res
-			.status(400)
-			.json({ ok: false, message: 'Parametro de busqueda incorrecto' })
-		return
-	}
-
-	const user = await User.findByPk(userId, {
-		include: [
-			{ model: Role, as: 'roleUser', attributes: ['id', 'name', 'code'] },
-			{
-				model: TypeDocument,
-				as: 'typeDocumentUser',
-				attributes: ['id', 'name', 'code'],
-			},
-		],
-	})
-
-	if (!user) {
-		res.json({ ok: false, message: 'Usuario no encontrado' })
-		return
-	}
-
-	if (user.roleUser.code !== 'Administrator') {
-		res.json({
-			ok: false,
-			message: 'No tiene permisos para enviar notificaciones',
-		})
-		return
-	}
-
-	const { to, title, body } = req.body
-
-	if (!EXPO_NOTIFICATION_URL) {
-		res.status(500).json({ ok: false, message: 'Error al enviar notificacion' })
-		return
-	}
-
-	const res2 = await fetch(EXPO_NOTIFICATION_URL, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Accept: 'application/json',
-		},
-		body: JSON.stringify({
-			to,
-			sound: 'default',
-			title,
-			body,
-		}),
-	})
-
-	const { data } = await res2.json()
-
-	if (data.status !== 'ok') {
-		res.status(500).json({ ok: false, message: 'Error al enviar notificacion' })
-		return
-	}
-
-	res.json({ ok: true, message: 'Notificacion enviada' })
-})
-
-user.get('/image/:userId', async (req, res) => {
-	const { userId } = req.params
-
-	const user = await User.findByPk(userId)
-	const imageUrl = `${imagesUrl}\\user\\${user?.imageUrl}`
-
-	if (!user || !fs.existsSync(imageUrl)) {
-		return res.sendFile(`${imagesUrl}\\base\\base_user.png`, { root: '.' })
-	}
-
-	return res.sendFile(imageUrl, { root: '.' })
+	// TODO -> Hacer esta ruta para permitir notificar a todos los usuarios | aprendices (con notification token activo), notificar solo a usuario.
 })
 
 user.put(
 	'/profile',
-	verifyToken2,
-	roleRequired(['Apprentice', 'Candidate', 'Administrator']),
+	sessionRequired,
+	roleRequired('*'),
+	validateRequest(updateProfileValidation),
 	upload.single('image'),
-	updateProfileValidation,
-	validateUser,
 	async (req: Request, res: Response) => {
-		const { userId } = req.headers
-
-		if (!userId || typeof userId !== 'string') {
-			res
-				.status(400)
-				.json({ ok: false, message: 'Parametro de busqueda incorrecto' })
-			return
-		}
-
-		const user = await User.findByPk(userId)
-
-		if (!user) {
-			res.json({ ok: false, message: 'Usuario no encontrado' })
-			return
-		}
-
+		const user = (req as RequestWithUser).user
 		const { name, lastname, email, phone } = req.body
-		// const image = req.file
 
-		user.name = name
-		user.lastname = lastname
-		user.phone = phone
-		user.email = email
+		// TODO -> Agregar las funciones de asociaciones en los tipos de los modelos
+		const profile = await Profile.findOne({ where: { userId: user.id } })
 
-		if (req.file?.path) {
-      const imagePath =  path.resolve(req.file.path)
-
-      const { ok, result } = await uploadImage(imagePath, {
-        public_id: user.id
-      })
-
-      fs.unlinkSync(imagePath)
-
-      if (!ok) {
-        res.json({ ok: false, message: 'Error subiendo la imagen' })
-        return
-      }
-      
-      // biome-ignore lint/style/noNonNullAssertion: It will be never null
-      user.imageUrl = result!.secure_url
-    }
-
-		await user.save()
-
-		res.json({ ok: true, message: 'Datos actualizados' })
-		return
-	}
-)
-
-user.post(
-	'/find-user',
-	findUserValidation,
-	validateUser,
-	async (req: Request, res: Response) => {
-		const { typeDocumentCode, document } = req.body
-		const typeDocument = await TypeDocument.findOne({
-			where: { code: typeDocumentCode },
-		})
-		const user = await User.findOne({
-			where: { typeDocument: typeDocument?.id, document },
-		})
-
-		if (!typeDocument) {
-			res.json({ ok: false, message: 'Tipo de documento no encontrado' })
-			return
-		}
-
-		if (!user) {
-			res.json({ ok: false, message: 'Usuario no encontrado' })
-			return
-		}
-
-		const newEmail = getSecretEmail(user.email, 2)
-		const newCodeData = user.resetPasswordData || {
-			timeNewCode: null,
-			timesCodeSent: 0,
-			code: null,
-		}
-		const timeNewCode = newCodeData.timeNewCode
-
-		res.json({ ok: true, user: { id: user.id, email: newEmail, timeNewCode } })
-	}
-)
-
-user.post(
-	'/send-password-reset-code',
-	sendResetCodeValidation,
-	validateUser,
-	async (req: Request, res: Response) => {
-		// TODO: Send email with code
-		const { userId } = req.body
-		const passwordResetCode = getPasswordResetCode(6)
-		const user = await User.findByPk(userId)
-
-		if (!user) {
-			res.json({ ok: false, message: 'Usuario no encontrado' })
-			return
-		}
-
-		const newCodeData = user.resetPasswordData || {
-			timeNewCode: null,
-			timesCodeSent: 0,
-			code: null,
-		}
-
-		if (
-			newCodeData.timeNewCode &&
-			new Date(newCodeData.timeNewCode) > new Date()
-		) {
-			res.json({
-				ok: false,
-				message: 'Tiempo de espera excedido',
-				timeNewCode: newCodeData.timeNewCode,
-			})
-			return
-		}
-
-		const timeCooldown =
-			newCodeData.timesCodeSent > 3 ? 3600 : newCodeData.timesCodeSent * 80
-		const timeNewCode = new Date()
-
-		timeNewCode.setSeconds(timeNewCode.getSeconds() + timeCooldown)
-
-		user.resetPasswordData = {
-			timeNewCode,
-			timesCodeSent: newCodeData.timesCodeSent + 1,
-			code: passwordResetCode,
-		}
-		await user.save()
-
-		setTimeout(() => {
-			if (user.resetPasswordData.code === passwordResetCode) {
-				user.resetPasswordData.code = null
-				user.save()
-			}
-		}, 21600)
-
-		setTimeout(() => {
-			user.resetPasswordData = {
-				timeNewCode: null,
-				timesCodeSent: 0,
-				code: null,
-			}
-		}, 86400)
-
-		res.json({ ok: true, timeNewCode })
-		return
-	}
-)
-
-user.post(
-	'/verify-password-reset-code',
-	verifyPasswordResetCodeValidation,
-	validateUser,
-	async (req: Request, res: Response) => {
-		const { userId, code } = req.body
-		const user = await User.findByPk(userId)
-
-		if (!user) {
-			res.json({ ok: false, message: 'Usuario no encontrado' })
-			return
-		}
-
-		if (user.resetPasswordData.code !== code) {
-			res.json({
-				ok: false,
-				message: 'El código de verificación no es correcto',
-			})
-			return
-		}
-
-		res.json({ ok: true })
-		return
-	}
-)
-
-user.put(
-	'/update-password',
-	updatePasswordValidation,
-	validateUser,
-	async (req: Request, res: Response) => {
-		const { userId, password, passwordConfirmation, code } = req.body
-		const user = await User.findByPk(userId)
-
-		if (!user) {
-			res.json({ ok: false, message: 'Usuario no encontrado' })
-			return
-		}
-
-		if (user.resetPasswordData.code !== code) {
-			res.json({
-				ok: false,
-				message: 'El código de verificación no es correcto',
-			})
-			return
-		}
-
-		if (password !== passwordConfirmation) {
-			res.json({
-				ok: false,
-				message: 'Las contraseñas no coinciden',
-				errors: {
-					path: 'passwordConfirmation',
-					message: 'Las contraseñas no coinciden',
-				},
-			})
-			return
-		}
-
-		user.password = bcrypt.hashSync(password, 10)
-		user.resetPasswordData.code = null
-		await user.save()
-
-		res.json({
-			ok: true,
-			message: 'Nueva contraseña actualizada',
-			urlReturn: 'login/',
-		})
-	}
-)
-
-user.post(
-	'/notify',
-	verifyToken2,
-  roleRequired('Administrator'),
-	notifyValidation,
-	validateUser,
-	async (req: Request, res: Response) => {
-		const { userId } = req.headers
-
-		if (!userId || typeof userId !== 'string') {
+		if (!profile) {
 			res
-				.status(400)
-				.json({ ok: false, message: 'Parametro de busqueda incorrecto' })
+				.status(404)
+				.json({ ok: false, message: 'Tu usuario no cuenta con un perfil, por favor contacta con el administrado' })
 			return
 		}
 
-		const user = await User.findByPk(userId, {
-			include: [
-				{ model: Role, as: 'roleUser', attributes: ['id', 'name', 'code'] },
-			],
-		})
-
-		const { title, body } = req.body
-
-		const role = await Role.findOne({ where: { code: 'Apprentice' } })
-
-		if (!role) {
+		try {
+			await Promise.all([
+				user.update({ email }),
+				profile.update({
+					name,
+					lastname,
+					phone
+				})
+			])
+		} catch (err) {
+			console.log(err)
 			res
 				.status(500)
-				.json({ ok: false, message: 'Ocurrio un error, intenta nuevamente' })
+				.json({ ok: false, message: 'Ocurrio un error actualizado tu perfil, por favor intenta más tarde' })
 			return
 		}
 
-		const allAprrentices = await User.findAll({
-			where: { role: role.id },
-			attributes: ['id', 'notificationToken'],
-		})
+		if (req.file?.path) {
+			const imagePath = path.resolve(req.file.path)
 
-		const tasks: unknown[] = []
+			const { ok, result } = await uploadImage(imagePath, {
+				public_id: user.id
+			})
 
-		for (const { notificationToken } of allAprrentices) {
-			if (!notificationToken || !EXPO_NOTIFICATION_URL) continue
-			tasks.push(
-				limit(() =>
-					fetch(EXPO_NOTIFICATION_URL, {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-							Accept: 'application/json',
-						},
-						body: JSON.stringify({
-							to: notificationToken,
-							sound: 'default',
-							title,
-							body,
-						}),
-					})
-				)
-			)
+			fs.unlinkSync(imagePath)
+
+			if (!ok) {
+				res.json({ ok: false, message: 'Error subiendo la imagen' })
+				return
+			}
+
+			// biome-ignore lint/style/noNonNullAssertion: It will be never null
+			await profile.update('imageUrl', result!.secure_url)
 		}
 
-		const results = await Promise.all(tasks)
-
-		console.log({ results })
-
-		res.json({ ok: true, message: 'Notificación enviada' })
+		res.json({ ok: true, message: 'Datos actualizados' })
 	}
 )
 
