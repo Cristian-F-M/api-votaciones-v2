972119d3d7ba8ca476a7914c054dd02c2fe97676
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-12 18:30:08 -0500

    Add shiftType to seed function

--- app/config/seed.ts
+++ app/config/seed.ts
@@ -1,12 +1,14 @@
-import { Role, TypeDocument, Config } from '@/models/index.js'
-import { CONFIGS, ROLES, TYPES_DOCUMENTS } from '@/constants/database'
+import { Role, TypeDocument, Config, ShiftType } from '@/models/index.js'
+import { CONFIGS, ROLES, SHIFT_TYPES, TYPES_DOCUMENTS } from '@/constants/database'
 
 async function seedDb() {
 	const typesDocuments = TypeDocument.bulkCreate(Object.values(TYPES_DOCUMENTS))
 	const roles = Role.bulkCreate(Object.values(ROLES))
 	const configs = Config.bulkCreate(Object.values(CONFIGS))
+  const shiftTypes = ShiftType.bulkCreate(Object.values(SHIFT_TYPES))
 
-	await Promise.all([typesDocuments, roles, configs])
+	await Promise.all([typesDocuments, roles, configs, shiftTypes])
+
 
 	console.log('Database seeded!!!!')
 }
