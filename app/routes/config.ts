53f28509af9f12e07f58c0e49543f6bbc577014a
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-16 15:22:51 -0500

    Fix routes and add another routes for update, delete and getAll configs

--- app/routes/config.ts
+++ app/routes/config.ts
@@ -1,34 +1,120 @@
+import { roleRequired, sessionRequired, validateRequest } from '@/middlewares/UserMiddlewares.js'
+import type { Config as ConfigModel } from '@/types/models'
 import express from 'express'
-import { Config } from '../models/index.js'
-import type { ConfigModel } from '@/types/models.js'
-import { roleRequired, verifyToken2 } from '@/middlewares/UserMiddlewares.js'
+import type { Request, Response } from 'express'
+import { Config } from '@/models'
+import { deleteOne, getOne, update } from '@/validators/config'
+import { Op } from 'sequelize'
 
 const config = express.Router()
 
-config.get('/', verifyToken2, roleRequired(['Apprentice', 'Candidate', 'Administrator']) ,async (req, res) => {
-	const { code, name } = req.query
+config.get(
+	'/:q',
+	sessionRequired,
+	roleRequired(['ADMINISTRATOR', 'APPRENTICE', 'CANDIDATE']),
+	validateRequest(getOne),
+	async (req: Request, res: Response) => {
+		const { q } = req.params
 
-	const query: Record<string, unknown> = {}
+		let config: ConfigModel | null = null
 
-	if (code) query.code = code
-	if (name) query.name = name
+		try {
+			config = await Config.findOne({ where: { [Op.or]: [{ code: q }, { id: q }] } })
 
-	let config: ConfigModel | ConfigModel[] | null
+			if (!config) {
+				res.status(404).json({ ok: false, message: 'No se encontro la configuración, por favor intenta nuevamente...' })
+				return
+			}
+		} catch (err) {
+			console.log(err)
+			res
+				.status(500)
+				.json({ ok: false, message: 'Ocurrio un error buscando la configuración, por favor intenta nuevamente...' })
+			return
+		}
 
-	if (code || name) {
-		config = await Config.findOne({
-			where: query,
-		})
-	} else {
-		config = await Config.findAll()
+		res.json({ ok: true, config })
 	}
+)
 
-	if (!config) {
-		res.status(404).json({ message: 'Config not found' })
-		return
+config.get(
+	'/all',
+	sessionRequired,
+	roleRequired(['ADMINISTRATOR', 'APPRENTICE', 'CANDIDATE']),
+	async (req: Request, res: Response) => {
+		try {
+			const configs = await Config.findAll()
+
+			res.json({ ok: true, configs })
+		} catch (err) {
+			console.log(err)
+			res
+				.status(500)
+				.json({ ok: false, message: 'Ocurrio un error buscando las configuraciones, por favor intenta nuevamente...' })
+			return
+		}
 	}
+)
 
-	res.json({ config })
-})
+config.put(
+	'/',
+	sessionRequired,
+	roleRequired('ADMINISTRATOR'),
+	validateRequest(update),
+	async (req: Request, res: Response) => {
+		const { id, name, description, value } = req.body
+
+		let config: ConfigModel | null = null
+
+		try {
+			config = await Config.findOne({ where: { id } })
+
+			if (!config) {
+				res.status(404).json({ ok: false, message: 'No se encontro la configuración, por favor intenta nuevamente...' })
+				return
+			}
+		} catch (err) {
+			console.log(err)
+			res
+				.status(500)
+				.json({ ok: false, message: 'Ocurrio un error buscando la configuración, por favor intenta nuevamente...' })
+			return
+		}
+
+		try {
+			await config.update({ name, description, value })
+			res.json({ ok: true, message: 'Configuración actualizada correctamente...', config })
+		} catch (err) {
+			console.log(err)
+			res
+				.status(500)
+				.json({ ok: false, message: 'Ocurrio un error actualizando la configuración, por favor intenta nuevamente...' })
+			return
+		}
+	}
+)
+
+config.delete(
+	'/:id',
+	sessionRequired,
+	roleRequired('ADMINISTRATOR'),
+	validateRequest(deleteOne),
+	async (req: Request, res: Response) => {
+		const { id } = req.params
+
+		try {
+			await Config.destroy({ where: { id } })
+
+			res.json({ ok: true, message: 'Configuración eliminada correctamente...' })
+			return
+		} catch (err) {
+			console.log(err)
+			res
+				.status(500)
+				.json({ ok: false, message: 'Ocurrio un error eliminando la configuración, por favor intenta nuevamente...' })
+			return
+		}
+	}
+)
 
 export default config
