c93a4d35ee7de8e9bf2355ae307f0a16f763e7a6
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-16 20:03:21 -0500

    Add some useful types

--- global.d.ts
+++ global.d.ts
@@ -7,3 +7,24 @@ declare module 'http' {
 		'session-type': ALLOWED_SESSION_TYPE
 	}
 }
+
+declare global {
+	type CustomOmit<T, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] }
+
+	type CustomPartial<T, K extends keyof T = keyof T> = {
+		[P in K]?: T[P] | undefined
+	} & {
+		[P in Exclude<keyof T, K>]: T[P]
+	}
+
+	type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+		{
+			[K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+		}[Keys]
+
+	type PartialExcept<T, K extends keyof T = never> = {
+		[P in K]: T[P]
+	} & {
+		[P in Exclude<keyof T, K>]?: T[P] | undefined
+	}
+}
