0c7ec622b7123e84c5081ddb40355e5169feee78
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-16 20:06:33 -0500

    Add function to merge n objects

--- lib/global.ts
+++ lib/global.ts
@@ -1,3 +1,8 @@
 export function getRandomNumber(min: number, max: number) {
 	return Math.floor(Math.random() * (max - min + 1)) + min
 }
+// biome-ignore lint/suspicious/noExplicitAny: It does not matter
+export function mergeObjects<T>(...options: Record<string, any>[]) {
+	const entries = options.flatMap((p) => Object.entries(p).filter((i) => i.every((e) => !!e)))
+	return Object.fromEntries(entries) as T
+}
