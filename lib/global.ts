140e546fe326a2d58da9ceed64442bfb090454cf
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-19 20:21:05 -0500

    Add function to get formatted seconds as string like `1 hour 10 minutes`

--- lib/global.ts
+++ lib/global.ts
@@ -6,3 +6,19 @@ export function mergeObjects<T>(...options: Record<string, any>[]) {
 	const entries = options.flatMap((p) => Object.entries(p).filter((i) => i.every((e) => !!e)))
 	return Object.fromEntries(entries) as T
 }
+
+const nf = new Intl.NumberFormat('es')
+
+export function formatTime(seconds: number) {
+	const h = Math.floor(seconds / 3600)
+	const m = Math.floor((seconds % 3600) / 60)
+	const s = seconds % 60
+
+	return [
+		h && `${nf.format(h)} hora${h !== 1 ? 's' : ''}`,
+		m && `${nf.format(m)} minuto${m !== 1 ? 's' : ''}`,
+		s && `${nf.format(s)} segundo${s !== 1 ? 's' : ''}`
+	]
+		.filter(Boolean)
+		.join(' ')
+}
