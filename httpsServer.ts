28a29140150480c3acb0663930a1769ce54e8b35
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-20 10:28:19 -0500

    Add log to print address and port from server

--- httpsServer.ts
+++ httpsServer.ts
@@ -5,6 +5,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { ConfigService } from '@/services/config.service.js'
 import morgan from 'morgan'
+import type { AddressInfo } from 'node:net'
 
 const PORT = process.env.PORT || 3000
 const app = createApp()
@@ -25,8 +26,11 @@ const options = {
 const server = https.createServer(options, app)
 
 server.listen(PORT, async () => {
+	const { address, port } = server.address() as AddressInfo
+
 	await ConfigService.load()
 
+	console.log(`Host: ${address}, Puerto: ${port} \n`)
 	console.log(`Server is running on port https://localhost:${PORT}`)
 	if (ip) console.log(`Server is running on port https://${ip}:${PORT}`)
 })
