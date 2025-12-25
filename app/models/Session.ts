2078f5d907d06a51c1e05a0d9f2babac3f71513d
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-24 20:59:13 -0500

    Add attrubute `isActive` to Session model and add paranoid configuration

--- app/models/Session.ts
+++ app/models/Session.ts
@@ -4,6 +4,7 @@ import type { Session as SessionModel } from '@/types/models'
 import User from '@/models/User'
 import { ALLOWED_SESSION_TYPES } from '@/constants/database'
 
+// TODO: Agregar la propiedad isActive y cuando se expire la sesión o cuando se cierre la sesión se marca como false, y al momento de buscar la sesión para validar la sesión del usuario se debe buscar agregando isActive = true
 const Session = sequelize.define<SessionModel>('Session', {
 	id: {
 		type: DataTypes.UUID,
@@ -18,6 +19,10 @@ const Session = sequelize.define<SessionModel>('Session', {
 		type: DataTypes.DATE,
 		allowNull: false
 	},
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
 	userId: {
 		type: DataTypes.UUID,
 		references: {
@@ -29,6 +34,6 @@ const Session = sequelize.define<SessionModel>('Session', {
 		type: DataTypes.ENUM(...Object.keys(ALLOWED_SESSION_TYPES)),
 		allowNull: false
 	}
-})
+}, { paranoid: true })
 
 export default Session
