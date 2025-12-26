4f889816433f5e1c65645499d31509ab331ae738
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-26 11:34:41 -0500

    Get args from the process to use https or not and to run both servers

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,39 @@ import type { Express } from 'express'
 import fs from 'node:fs'
 import path from 'node:path'
 
-export const CERTIFCATE_NAME = process.env.CERTIFCATE_NAME
 const args = process.argv
 const isSecure = args.includes('--secure')
-const PORT = isSecure ? 8080 : 8081
+export const runBothServers = args.includes('--both')
+
+if (isSecure && runBothServers) {
+	throw new Error('Cannot set --secure and --both at the same time')
+}
+
+const CERTIFCATE_NAME = process.env.CERTIFCATE_NAME
+const securePort = 8080
+const defaultPort = 8081
+const PORT = isSecure ? securePort : defaultPort
 const app = createApp()
 const ip = getIp()
 const protocol = isSecure ? 'https' : 'http'
-const options = {
-	key: fs.readFileSync(path.join(__dirname, `certificates/${CERTIFCATE_NAME}-key.pem`)),
-	cert: fs.readFileSync(path.join(__dirname, `certificates/${CERTIFCATE_NAME}.pem`))
-}
 
 let server: https.Server | Express = app
 
-if (isSecure) {
-	app.use(morgan('dev'))
-	server = https.createServer(options, app)
+if (isSecure || runBothServers) {
+	const options = {
+		key: fs.readFileSync(path.join(__dirname, `certificates/${CERTIFCATE_NAME}-key.pem`)),
+		cert: fs.readFileSync(path.join(__dirname, `certificates/${CERTIFCATE_NAME}.pem`))
+	}
+
+	const httpsServer = https.createServer(options, app)
+	if (runBothServers) httpsServer.listen(securePort, () => listeningListener(securePort, 'https'))
+	if (isSecure) server = httpsServer
 }
 
-server.listen(PORT, () => {
-	console.log(`Server is running on port ${protocol}://localhost:${PORT}`)
-	if (ip) console.log(`Server is running on port ${protocol}://${ip}:${PORT}`)
-})
+server.listen(PORT, () => listeningListener(PORT, protocol))
+
+function listeningListener(port: number, protocol: string) {
+	console.log(`\x1b[36m[${protocol.toUpperCase()}]\x1b[0m`)
+	console.log(`Server is running on port ${protocol}://localhost:${port}`)
+	if (ip) console.log(`Server is running on port ${protocol}://${ip}:${port}`)
+}
