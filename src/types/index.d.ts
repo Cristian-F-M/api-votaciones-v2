dcbfe21a45d52caf5f6c97ea67536e0208d0f658
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-30 21:27:20 -0500

    Add type for device type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,8 @@
-import type { ALLOWED_SESSION_TYPES } from '@/constants/database'
+import type { ALLOWED_DEVICE_TYPES, ALLOWED_SESSION_TYPES } from '@/constants/database'
 import type { User } from '@/types/models'
 import type { Request } from 'express'
 
 export type CustomRequest<T extends Record<string, unknown>> = Request & T
 export type AllowedSessionTypes = keyof typeof ALLOWED_SESSION_TYPES
 export type ConfigScope = 'system' | 'user'
+export type AllowedDeviceTypes = keyof typeof ALLOWED_DEVICE_TYPES
