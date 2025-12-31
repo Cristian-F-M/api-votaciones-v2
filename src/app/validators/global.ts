b7581cd41e73718647e7a7d582bdb0febf7e4d59
Cristian-F-M <cfmorales.diaz@gmail.com>
2025-12-30 21:28:23 -0500

    Add validation for Device-Type header

--- src/app/validators/global.ts
+++ src/app/validators/global.ts
@@ -1,4 +1,4 @@
-import { ALLOWED_SESSION_TYPES } from '@/constants/database'
+import { ALLOWED_SESSION_TYPES, ALLOWED_DEVICE_TYPES } from '@/constants/database'
 import { header } from 'express-validator'
 
 export const globalValidator = [
@@ -9,5 +9,13 @@ export const globalValidator = [
 		.withMessage('Solicitud realizada incorrectamente, por favor intenta nuevamente')
 		.toUpperCase()
 		.custom((input, meta) => Object.keys(ALLOWED_SESSION_TYPES).includes(input))
+		.withMessage('Solicitud realizada incorrectamente, por favor intenta nuevamente'),
+	header('Device-Type')
+		.notEmpty()
+		.withMessage('Solicitud realizada incorrectamente, por favor intenta nuevamente')
+		.isString()
+		.withMessage('Solicitud realizada incorrectamente, por favor intenta nuevamente')
+		.toUpperCase()
+		.custom((input, meta) => Object.keys(ALLOWED_DEVICE_TYPES).includes(input))
 		.withMessage('Solicitud realizada incorrectamente, por favor intenta nuevamente')
 ]
